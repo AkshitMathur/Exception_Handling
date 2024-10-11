e549388f2ab75138ce6d4010d587f2f6c30ed9e3
AkshitMathur <mathur.aksh2712@gmail.com>
2024-10-11 11:33:33 +0530

    uploaded

---
+++ Eligibility_for_Voting.cpp
@@ -0,0 +1,26 @@
+// 23070123014 Akshit Mathur
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout<<"enter the age for voting:"<<endl;
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+    else{
+        cout<<"you are eligible for voting"<<endl;
+    }
+}
+catch(int age){
+    cout<<"\nERROR:You are not eligible for voting at:"<<age<<endl;
+}
+    return 0;
+}
+//Output
+/*enter the age for voting:
+14
+
+ERROR:You are not eligible for voting at:14*/
